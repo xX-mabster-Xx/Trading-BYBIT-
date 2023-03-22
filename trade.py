cf87b36666abdbabee17b8430f43cd871609ff6b
mabster <s22d_usolcev@179.ru>
2023-03-23 01:22:02 +0300

    ok

--- trade.py
+++ trade.py
@@ -3,16 +3,26 @@ import talib
 from talib import abstract
 import yfinance as yf
 
+from tvDatafeed import TvDatafeed, Interval
+import matplotlib.pyplot as plt
+username = 'x-mabster-x'
+password = 'qwerty123'
+
+tv = TvDatafeed(username, password)
 
 def solve(index: str) -> float:
-    data = yf.download(index, start='2022-03-21')
-    print(data)
-    # data.rename(columns= {'O'})
+    data = tv.get_hist(symbol= index, exchange='BYBIT', interval=Interval.in_1_minute, n_bars=1500)
+    # print(data)
+    sma = abstract.NATR(data)
+    # print(sma.dtypes)
+    data.plot(kind='line',y='open', color='red', )
+    plt.show()
+    # print(talib.get_function_groups())
     return 0
 
 
 if __name__ == '__main__':
-    solve('TSLA')
+    solve('BTCUSDT.P')
 
 
 
